7a925b903139d436720ed8ac96bb210995e7a4a1
fatenalsahli <97634511+fatenalsahli@users.noreply.github.com>
2022-03-20 10:19:59 +0300

    Test

---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,26 @@
+class HelloWorld 
+    def say_hello
+        return "Hello world!!"
+    end
+
+end
+
+
+describe HelloWorld do 
+    context "Vaildate say hello function" do
+
+        it "Show say hello world when call say hello function" do
+            h1 = HelloWorld.new()
+            expect(h1.say_hello).to eq "Hello world!!"
+    
+        end
+    
+        it "Show not say hello world when call say hello function" do
+            h1 = HelloWorld.new()
+            expect(h1.say_hello).not_to eq "Random!!"
+    
+        end
+
+    end
+    
+end
